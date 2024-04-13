24ca2f8708c61a687fa506fdd89df11c11f177e6
Harsh Gaur <hgaur701@gmail.com>
2024-04-13 18:18:25 +0530

    fixing url in extension's side

--- client/src/popup.js
+++ client/src/popup.js
@@ -1,4 +1,4 @@
-const url = 'http://localhost:5000/'
+const url = 'https://capybara-yt-extension.vercel.app/'
 const formContainer = document.getElementById('formContainer');
 const authorizedContainer = document.getElementById('authorizedContainer');
 
